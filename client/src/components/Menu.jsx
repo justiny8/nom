4161e0e619254f41410f12097399d4dc697f6e10
justiny8 <104675479+justiny8@users.noreply.github.com>
2022-10-23 20:09:59 -0700

    Add files via upload

---
+++ client/src/components/Menu.jsx
@@ -0,0 +1,58 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const Menu = ({cat}) => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts/?cat=${cat}`);
+        setPosts(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [cat]);
+  // const posts = [
+  //   {
+  //     id: 1,
+  //     title: "nom nom nom",
+  //     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+  //     img: "https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "nom nom nom",
+  //     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+  //     img: "https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "nom nom nom",
+  //     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+  //     img: "https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  //   },
+  //   {
+  //     id: 4,
+  //     title: "nom nom nom",
+  //     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+  //     img: "https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  //   },
+  // ];
+  return (
+    <div className="menu">
+      <h1>Other posts you may like</h1>
+      {posts.map((post) => (
+        <div className="post" key={post.id}>
+          <img src={`../upload/${post?.img}`} alt="" />
+          <h2>{post.title}</h2>
+          <button>Read More</button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Menu;
